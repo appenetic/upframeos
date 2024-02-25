58262f53e3a65a180dfbd6d03ff23534729b0083
Sascha Held <saschaheld@Saschas-MBP.lan>
2024-02-25 13:13:12 +0100

    FIX: Settings

--- app/app/models/settings.rb
+++ app/app/models/settings.rb
@@ -3,8 +3,8 @@ class Settings < RailsSettings::Base
   cache_prefix { "v1" }
 
   scope :general do
-    field :orientation, default: "portrait", validates: { presence: true, inclusion: { in: orientations } }, option_values: orientations
-    field :wifi_country, default: "de", validates: { presence: true, inclusion: { in: wifi_country_codes } }, option_values: wifi_country_codes
+    field :orientation, default: "portrait", validates: { presence: true, inclusion: { in: Settings.orientations } }, option_values: Settings.orientations
+    field :wifi_country, default: "de", validates: { presence: true, inclusion: { in: Settings.wifi_country_codes } }, option_values: Settings.wifi_country_codes
     field :wifi_ssid
     field :wifi_password
   end
@@ -13,15 +13,15 @@ class Settings < RailsSettings::Base
     field :canvas_feature_enabled, type: :boolean, default: true
   end
 
-  def orientations
+  def self.orientations
     %w[portrait portrait_inverted landscape landscape_inverted]
   end
 
-  def wifi_country_codes
+  def self.wifi_country_codes
     %w[gb fr de us se]
   end
   
-  def unix_orientation(orientation)
+  def self.unix_orientation(orientation)
     case orientation
     when 'portrait'
       'left'
@@ -36,7 +36,7 @@ class Settings < RailsSettings::Base
     end
   end
 
-  def wpa_supplicant_content
+  def self.wpa_supplicant_content
     country = Settings.wifi_country
     ssid = Settings.wifi_ssid
     psk = Settings.wifi_password
