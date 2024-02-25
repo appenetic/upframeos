f0d25a8af8d209b0ea8504d3308b1d6aec5c2002
Sascha Held <saschaheld@Saschas-MacBook-Pro.local>
2024-02-25 15:36:43 +0100

    FIX: Script

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Starting up..."
 
-sh ~/upframe/scripts/create_setup_wifi.sh &
-sh ~/upframe/scripts/start_browser.sh
+sh ~/upframeos/scripts/create_setup_wifi.sh &
+sh ~/upframeos/scripts/start_browser.sh
