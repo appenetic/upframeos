a2985276309ca3abdeacf38666c46881fc7e1ff1
Sascha Held <saschaheld@Saschas-MacBook-Pro.local>
2024-02-25 11:33:31 +0100

    CHANGE: Initial setup scripts

--- scripts/initial_setup.sh
+++ scripts/initial_setup.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+
+# Install required packages
 sudo apt-get install matchbox chromium unclutter xorg git
+
+# Create upframe user account
+sudo useradd -m -s /bin/bash -G sudo -p $(openssl passwd -1 upframe) upframe
