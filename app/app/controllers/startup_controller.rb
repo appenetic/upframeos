c0aaf2d5b94d1ada83925716fb7670d045de60f1
Sascha Held <saschaheld@Saschas-MBP.lan>
2024-02-27 20:51:51 +0100

    FIX: Settings

--- app/app/controllers/startup_controller.rb
+++ app/app/controllers/startup_controller.rb
@@ -2,7 +2,7 @@ require_relative '../services/qrcode_service.rb'
 class StartupController < ApplicationController
   def index
     local_ip = '192.168.1.1'
-    url = "http://#{local_ip}:3000/admin/"
+    url = "http://#{local_ip}:3000/admin/settings/"
 
     qr_code_service = QRCodeService.new
     @qr_svg = qr_code_service.generate(url)
