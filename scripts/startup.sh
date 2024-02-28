eda973d6941eefeb3a602e6e28c220377551eb82
Sascha Held <saschaheld@MBP-von-Sascha.lan>
2024-02-28 23:00:31 +0100

    FIX: Startup

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -5,6 +5,7 @@ sleep 1
 cd /home/upframe/upframeos && git -c safe.directory=/home/upframe/upframeos pull || echo "git pull failed, but script continues"
 sh /home/upframe/upframeos/scripts/create_setup_wifi.sh &
 
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 RAILS_ENV=production bundle exec rake db:create
 RAILS_ENV=production bundle exec rake db:migrate
 RAILS_ENV=production bundle exec rake assets:precompile
