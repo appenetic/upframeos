97c6c4d16f2eda24ec67d0efbc02b2cca84b3a57
Sascha Held <saschaheld@MBP-von-Sascha.lan>
2024-02-28 23:15:47 +0100

    FIX: Startup

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -5,13 +5,6 @@ sleep 1
 cd /home/upframe/upframeos && git -c safe.directory=/home/upframe/upframeos pull || echo "git pull failed, but script continues"
 sh /home/upframe/upframeos/scripts/create_setup_wifi.sh &
 
-if [[ -s "$HOME/.rvm/scripts/rvm" ]] ; then
-  source "$HOME/.rvm/scripts/rvm"
-elif [[ -s "/usr/local/rvm/scripts/rvm" ]] ; then
-  source "/usr/local/rvm/scripts/rvm"
-else
-  echo "RVM not found"
-fi
 
 export PATH="$PATH:$HOME/.rvm/bin"
 source /home/upframe/.bash_profile || source /home/upframe/.bashrc
