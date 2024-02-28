b0eec052959096bc4601b7c378c494c9cfbe3d73
Sascha Held <saschaheld@MBP-von-Sascha.lan>
2024-02-28 21:32:27 +0100

    FIX: Startup script

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -2,6 +2,6 @@
 echo "Starting up..."
 sleep 1
 
-cd ~/upframeos && git pull || echo "git pull failed, but script continues"
-sh ~/upframeos/scripts/create_setup_wifi.sh &
-sudo xinit ~/upframeos/scripts/start_browser.sh
+cd /home/upframe/upframeos && git pull || echo "git pull failed, but script continues"
+sh /home/upframe/upframeos/scripts/create_setup_wifi.sh &
+sudo xinit /home/upframe/upframeos/scripts/start_browser.sh
