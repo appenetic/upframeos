8f02bd0e321b82f6504bd9cb94847562b3546564
Sascha Held <saschaheld@MBP-von-Sascha.lan>
2024-02-28 23:48:23 +0100

    FIX: Startup script

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -5,7 +5,9 @@ sleep 1
 cd /home/upframe/upframeos && git -c safe.directory=/home/upframe/upframeos pull || echo "git pull failed, but script continues"
 #sh /home/upframe/upframeos/scripts/create_setup_wifi.sh &
 
+echo $PATH
 export PATH="$PATH:/home/upframe/.rvm/bin"
+echo $PATH
 source /home/upframe/.bash_profile || source /home/upframe/.bashrc
 
 cd /home/upframe/upframeos/app/
