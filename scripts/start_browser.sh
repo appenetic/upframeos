5f6b1e429d43a6a1bba8c94cd7f1652012df4956
Sascha Held <saschaheld@MBP-von-Sascha.lan>
2024-02-28 22:26:24 +0100

    FIX: Startup script

--- scripts/start_browser.sh
+++ scripts/start_browser.sh
@@ -5,4 +5,4 @@ xset s noblank # don't blank the video device
 matchbox-window-manager -use_titlebar no &
 xrandr --output HDMI-1 --rotate $(cat config/orientation.cfg) &
 unclutter &
-chromium --noerrdialogs --kiosk --check-for-update-interval=31536000 --no-sandbox --ignore-gpu-blacklist http://localhost:3000
+chromium --noerrdialogs --kiosk --check-for-update-interval=31536000 --no-sandbox --ignore-gpu-blacklist http://localhost:3000/startup
