acb402b2eb5802d8c08b27cee75bf9169eca6df1
Sascha Held <saschaheld@MBP-von-Sascha.lan>
2024-02-28 23:31:24 +0100

    ADD: PATH

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -6,6 +6,9 @@ sleep 1
 cd /home/upframe/upframeos && git -c safe.directory=/home/upframe/upframeos pull || echo "git pull failed, but script continues"
 #sh /home/upframe/upframeos/scripts/create_setup_wifi.sh &
 
+export PATH="$PATH:$HOME/.rvm/bin"
+source /home/upframe/.bash_profile || source /home/upframe/.bashrc
+
 cd /home/upframe/upframeos/app/
 RAILS_ENV=production bundle exec rake db:create || true
 RAILS_ENV=production bundle exec rake db:migrate || true
