d9287c5ef912eef4df89060fafc9311fc01d45bb
Sascha Held <saschaheld@MBP-von-Sascha.lan>
2024-02-29 02:34:07 +0100

    FIX: Startup

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -15,6 +15,6 @@ cd /home/upframe/upframeos/app/
 RAILS_ENV=production bundle exec rake db:create
 RAILS_ENV=production bundle exec rake db:migrate
 RAILS_ENV=production bundle exec rake assets:precompile
-RAILS_ENV=production bundle exec rails s &
+nohup RAILS_ENV=production bundle exec rails s > /dev/null 2>&1 &
 
 sudo xinit /home/upframe/upframeos/scripts/start_browser.sh
