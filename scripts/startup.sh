24a384a74ca7fab60f40209e114f52a38497eafe
Sascha Held <saschaheld@MacBook-Pro-von-Sascha.local>
2024-02-29 11:14:52 +0100

    FIX: Startup script

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -15,17 +15,39 @@ cd /home/upframe/upframeos/app/
 RAILS_ENV=production bundle exec rake db:create
 RAILS_ENV=production bundle exec rake db:migrate
 RAILS_ENV=production bundle exec rake assets:precompile
-RAILS_ENV=production bundle exec rails s -d
 
-wait_for_server() {
-    echo "Waiting for Rails server to start..."
-    while ! nc -z localhost 3000; do
-      echo "waiting for application server to become responsive"
-      sleep 1 # wait for 1 second before check again
-    done
+start_server() {
+    RAILS_ENV=production bundle exec rails s -d
     echo "Rails server started."
 }
 
+# Function to wait for the server to become responsive
+wait_for_server() {
+    while true; do
+        echo "Waiting for Rails server to start..."
+        while ! nc -z localhost 3000; do
+            echo "Waiting for application server to become responsive..."
+            sleep 1 # wait for 1 second before checking again
+        done
+
+        echo "Rails server is responsive."
+
+        # Server is up; now checking continuously for responsiveness
+        while nc -z localhost 3000; do
+            echo "Server is up and running."
+            sleep 10 # check every 10 seconds to reduce load
+        done
+
+        echo "Server is not responsive. Attempting to restart."
+        # Kill the existing server process before restarting
+        # This step depends on how you're running your Rails server.
+        # For example, you might need to kill a specific PID or use pkill to kill the process by name
+        # pkill -f 'rails' # Uncomment and adjust according to your server process
+        start_server # Restart the server
+    done
+}
+
+start_server
 wait_for_server
 
-sudo xinit /home/upframe/upframeos/scripts/start_browser.sh
+#sudo xinit /home/upframe/upframeos/scripts/start_browser.sh
