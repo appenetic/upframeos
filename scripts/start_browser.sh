999fa8733eb76d3b69612f2cb76eb60d277044e1
Sascha Held <saschaheld@MacBook-Pro-von-Sascha.local>
2024-02-29 11:50:06 +0100

    FIX: Start browser

--- scripts/start_browser.sh
+++ scripts/start_browser.sh
@@ -1,8 +1,33 @@
-#!/bin/sh
-xset -dpms # disable DPMS (Energy Star) features.
-xset s off # disable screen saver
-xset s noblank # don't blank the video device
-matchbox-window-manager -use_titlebar no &
-xrandr --output HDMI-1 --rotate $(cat config/orientation.cfg) &
-unclutter &
-chromium --noerrdialogs --kiosk --check-for-update-interval=31536000 --no-sandbox --ignore-gpu-blacklist http://localhost/startup
+#!/bin/bash
+
+# URL to check for Rails app readiness
+CHECK_URL="http://localhost:3000"
+
+# Function to check if the Rails app is ready
+check_rails_ready() {
+    while true; do
+        if curl --silent --fail $CHECK_URL; then
+            echo "Rails app is ready."
+            return 0
+        else
+            echo "Rails app is not ready yet. Retrying..."
+            sleep 5 # Wait for 5 seconds before retrying
+        fi
+    done
+}
+
+# Wait for Rails app to become ready
+if check_rails_ready; then
+    # Prepare the display environment
+    xset -dpms # disable DPMS (Energy Star) features.
+    xset s off # disable screen saver
+    xset s noblank # don't blank the video device
+
+    # Start window manager and set display properties
+    matchbox-window-manager -use_titlebar no &
+    xrandr --output HDMI-1 --rotate $(cat config/orientation.cfg) &
+    unclutter &
+
+    # Start the browser
+    chromium --noerrdialogs --kiosk --check-for-update-interval=31536000 --no-sandbox --ignore-gpu-blacklist http://localhost/startup
+fi
