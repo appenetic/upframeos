54b6db79567e7158136c2b5193bef8946f8c1b40
Sascha Held <saschaheld@MBP-von-Sascha.lan>
2024-02-29 14:00:27 +0100

    FIX: Crash

--- app/config/initializers/rspotify.rb
+++ app/config/initializers/rspotify.rb
@@ -1,4 +1,8 @@
-RSpotify::authenticate(
-    Rails.configuration.upframe_api.spotify_client_id, 
+begin
+  RSpotify::authenticate(
+    Rails.configuration.upframe_api.spotify_client_id,
     Rails.configuration.upframe_api.spotify_client_secret
-)
+  )
+rescue => e
+  Rails.logger.error "Failed to authenticate with Spotify: #{e.message}"
+end
