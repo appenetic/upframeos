c635fef846149db0e5bf38c4fee87f84aa6a638b
Sascha Held <saschaheld@MBP-von-Sascha.lan>
2024-02-29 03:02:12 +0100

    FIX: Statup

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -12,9 +12,9 @@ source /home/upframe/.bash_profile || source /home/upframe/.bashrc
 source /home/upframe/.rvm/scripts/rvm
 
 cd /home/upframe/upframeos/app/
-#RAILS_ENV=production bundle exec rake db:create
-#RAILS_ENV=production bundle exec rake db:migrate
-#RAILS_ENV=production bundle exec rake assets:precompile
+RAILS_ENV=production bundle exec rake db:create
+RAILS_ENV=production bundle exec rake db:migrate
+RAILS_ENV=production bundle exec rake assets:precompile
 RAILS_ENV=production bundle exec rails s -d
 
 wait_for_server() {
