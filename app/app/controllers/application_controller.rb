891fd99edfbfeead980618db5dfba8aea73f2b00
Sascha Held <saschaheld@MacBook-Pro-von-Sascha.local>
2024-03-02 17:41:40 +0100

    FIX: Access control issues

--- app/app/controllers/application_controller.rb
+++ app/app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :add_cors_headers
+
+  private
+  def add_cors_headers
+    response.set_header('Access-Control-Allow-Origin', '*')
+    response.set_header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+    response.set_header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  end
 end
