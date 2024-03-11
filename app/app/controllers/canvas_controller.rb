81a68d5f5b50b258181442c2e819d7671ac1e145
Sascha Held <saschaheld@MBP-von-Sascha.lan>
2024-03-11 21:42:07 +0100

    FIX: Performance

--- app/app/controllers/canvas_controller.rb
+++ app/app/controllers/canvas_controller.rb
@@ -8,20 +8,33 @@ class CanvasController < ApplicationController
   before_action :playback_active?
   before_action :playback_active?, except: [:playing_status]
 
+
   def current_track
-    @player = initialize_player
     retries = 0
+    track_uri = @player&.currently_playing&.uri
+
+    # Check cache first
+    cached_data = Rails.cache.read(track_uri)
+    if track_uri && cached_data && Time.now - cached_data[:timestamp] < 1800
+      render json: cached_data[:response]
+      return
+    end
+
     begin
       if @player.present? && @player.respond_to?(:currently_playing) && @player.currently_playing
-        render json: {
+        response_body = {
           artist_name: @player.currently_playing.artists.first.name,
           album_name: @player.currently_playing.album.name,
           track_name: @player.currently_playing.name,
           cover_image_url: @player.currently_playing.album.images.first['url'],
-          canvas_url: SpotifyCanvasService.instance.fetch_canvas_url(@player.currently_playing.uri),
+          canvas_url: SpotifyCanvasService.instance.fetch_canvas_url(track_uri),
           background_color: extract_main_color(@player.currently_playing.album.images.first['url']),
           reload_after_ms: (@player.currently_playing.duration_ms - @player.progress) + 2000
         }
+
+        Rails.cache.write(track_uri, { response: response_body, timestamp: Time.now })
+
+        render json: response_body
       else
         render json: { error: 'No track currently playing' }, status: :not_found
       end
