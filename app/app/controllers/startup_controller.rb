ff44292094b2b9bdbc8338ffacea38fbbe3d3323
Sascha Held <saschaheld@MBP-von-Sascha.lan>
2024-03-21 22:06:35 +0100

    FIX: Startup controller

--- app/app/controllers/startup_controller.rb
+++ app/app/controllers/startup_controller.rb
@@ -1,5 +1,5 @@
 require 'net/http'
-require_relative '../services/qrcode_service.rb'
+require_relative '../services/qrcode_service'
 
 class StartupController < ApplicationController
   def index
@@ -8,9 +8,7 @@ class StartupController < ApplicationController
       # Redirect to the root route if there's internet connectivity
       redirect_to root_url
     else
-      # Proceed with the original logic if there's no internet connectivity
-      local_ip = '192.168.1.1'
-      url = "http://#{local_ip}:3000/admin/settings/"
+      url = 'http://upframe.local/admin/settings/'
 
       qr_code_service = QRCodeService.new
       @qr_svg = qr_code_service.generate(url)
@@ -21,10 +19,8 @@ class StartupController < ApplicationController
 
   def internet_connection?
     begin
-      # Try to connect to a reliable website
       true if Net::HTTP.get(URI('http://www.google.com'))
     rescue
-      # Return false if the connection attempt fails
       false
     end
   end
