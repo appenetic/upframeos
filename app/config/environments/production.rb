3575c1a310707a770b897087061104e8cce05ad1
Sascha Held <saschaheld@MacBook-Pro-von-Sascha.local>
2024-04-23 12:33:25 +0200

    FIX: Caching not working

--- app/config/environments/production.rb
+++ app/config/environments/production.rb
@@ -65,7 +65,10 @@ Rails.application.configure do
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  config.cache_store = :memory_store
+  config.public_file_server.headers = {
+    "Cache-Control" => "public, max-age=#{2.days.to_i}"
+  }
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
