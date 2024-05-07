08b3419a695ad00cb9451ac2233d0bf712b00404
Sascha Held <saschaheld@MBP-von-Sascha.lan>
2024-05-07 23:17:09 +0200

    ADD: Dashboard

--- app/app/admin/dashboard.rb
+++ app/app/admin/dashboard.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 ActiveAdmin.register_page "Dashboard" do
-  menu priority: 0, label: proc { I18n.t("active_admin.dashboard") }, if: proc { false }
+  menu priority: 0, label: proc { I18n.t("active_admin.dashboard") }, if: proc { true }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
-    end
+    render partial: 'layouts/admin/dashboard'
+  end
+
+  controller do
+
   end
 end
--- app/app/services/upframe_update_service.rb
+++ app/app/services/upframe_update_service.rb
@@ -1,14 +1,12 @@
 class UpframeUpdateService
   SCRIPT_DIR = "/home/upframe/upframeos/scripts"
-  LOG_FILE = "/home/upframe/update.log"
-
   def self.update
-    system("#{SCRIPT_DIR}/update.sh >> #{LOG_FILE} 2>&1")
+    system("#{SCRIPT_DIR}/update.sh 2>&1")
     case $?.exitstatus
     when 0
       :no_updates
     when 2
-      system("sudo systemctl restart weston upframe &>> #{LOG_FILE}")
+      system("sudo systemctl restart weston upframe")
       :updates_applied
     else
       :failed
